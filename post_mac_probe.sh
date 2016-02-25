7a83df09bb36d43aa5dfbd079175b800e5bcb43e
TianyuanPan <TianyuanPan@localhost.localdomain>
2016-02-25 12:08:01 +0800

    change json format, MAC NOT only

--- post_mac_probe.sh
+++ post_mac_probe.sh
@@ -14,32 +14,42 @@ DEVICE_ID=""
 get_date_to_post_data_file()
 {
   tmp=/tmp/.tmpdat_
-  echo "" > $POST_DATA_FILE
-  time=$(date +'%Y-%m-%d %H:%M:%S') 
-  cat $PROBE_MAC_FILE | grep -v 00:00:00:00:00:00 | awk '{print $1,$2" t"}' > $tmp 
-  while read line; do echo ${line//t/$time} >> $POST_DATA_FILE; done < $tmp 
+  cat /dev/null > $POST_DATA_FILE 
+  cat $PROBE_MAC_FILE | grep -v 00:00:00:00:00:00 | awk '{print $1,$2}' > $tmp 
+  while read line; do echo "$line" >> $POST_DATA_FILE; done < $tmp 
 }
 
 set_device_id()
 {
-   DEVICE_ID=$(ifconfig|grep -e eth0|sed -n "1p"|awk '{print $NF}'|tr -d ":")
+   DEVICE_ID=$(ifconfig|grep -e eth0|awk '{print $NF}'|tr -d ":"|tr [a-z] [A-Z])
 }
 
 make_json_data()                                                       
 {                                                                      
-  tmp=/tmp/.probe_post_data.txt.tmp                                    
-  cat $POST_DATA_FILE | tr -d "[" | tr -d "]" | awk  '!a[$2]++' | grep -v "^$" > $tmp 
+  tmp=/tmp/.probe_post_data.txt.tmp   
+  time=$(date +'%Y-%m-%d %H:%M:%S')
+
+#  MAC only
+#  cat $POST_DATA_FILE | tr -d "[" | tr -d "]" | awk  '!a[$2]++' | grep -v "^$" > $tmp 
+# MAC NOT only
+  cat $POST_DATA_FILE | tr -d "[" | tr -d "]" | grep -v "^$" > $tmp
   
   old=$(cat $COUNT_FILE)
   deta=$(cat $tmp | wc -l)
   new=$(($old+$deta))
   echo $new > $COUNT_FILE
   
-  json_data="{\"id\":\"$DEVICE_ID\",\"count\":$new,\"data\":["                                                                               
+  json_data="{\"id\":\"$DEVICE_ID\",\"count\":$new,\"time\":\"$time\",\"data\":["                                                                               
   json_tmp=""                                                                                                                 
-  json_tmp2=""                                                                                                                
-  while read line;do json_tmp=$(echo $line | awk '{print  "{\"time\":\""$3,$4"\",\"rssi\":",$1",""\"mac\":","\""$2"\"},"}'); \
-                     json_tmp2="$json_tmp2$json_tmp";json_tmp="";done < $tmp
+  json_tmp2=""  
+  
+  while read line
+  do 
+    json_tmp=$(echo $line | awk '{print "{\"mac\":\""$2"\",","\"rssi\":"$1"},"}')
+    json_tmp2="$json_tmp2$json_tmp"
+    json_tmp=""
+  done < $tmp
+  
  json_data="$json_data$json_tmp2"                                      
  json_data="${json_data%,}]}"                            
  rm -f $tmp                                              
@@ -69,24 +79,24 @@ get_url_and_checkInterval()
 #                                                                                                                             
 main_loop()                                                                                                                   
 {                                                                                                                             
-  echo "CheckInterval: $1"                                                  
+  echo "CheckInterval: $1"
   echo "TheUrl: $2" 
-  if [ -z $1 ]                                                              
+  if [ -z $1 ]  
   then                                                                      
-    echo "CheckInterval is NULL, Parameter ERROR! exit(1)"                  
+    echo "CheckInterval is NULL, Parameter ERROR! exit(1)"
     exit 1                                                
-  fi                                                      
+  fi        
   if [ -z $2 ]                                            
   then                                                                                                                        
-    echo "Url is NULL, Parameter ERROR! exit(2)"                                                                              
+    echo "Url is NULL, Parameter ERROR! exit(2)"
     exit 2                                                                                                                    
   fi
   while [ true ]                                                            
   do                                                                        
     get_date_to_post_data_file                                           
     make_json_data 
-    post_data_to_server $2                                
-    rm -rf  $POST_DATA_FILE
+    post_data_to_server $2
+#    rm -rf  $POST_DATA_FILE
     sleep $1
   done                                                                                                                        
 }                                                                                                                             
